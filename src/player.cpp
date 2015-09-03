09587f1a902164abb9d0480f58867c4ebd46de54
Frans Skarman <frans.skarman@gmail.com>
2015-09-03 09:21:40 +0200

    Added basic keyboard controlls

--- src/player.cpp
+++ src/player.cpp
@@ -15,13 +15,22 @@ void Player::update(float time)
 
     HumanEntity::movementAmount = sf::Joystick::getAxisPosition(0, sf::Joystick::X) / 100.0f;
 
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+    {
+        movementAmount = 1;
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+    {
+        movementAmount = -1;
+    }
+
     //Deadzone
     if(std::abs(movementAmount) < 0.5)
     {
         movementAmount = 0;
     }
 
-    if(sf::Joystick::isButtonPressed(0, 0))
+    if(sf::Joystick::isButtonPressed(0, 0) || sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
     {
         jump(600);
     }
